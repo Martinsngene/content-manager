b561664eb0f172106689f0d049a40f6a00f60ab1
Martins Ngene <martinsngene9@gmail.com>
2022-12-28 14:13:24 +0100

    update: Compare getStaticProps and getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,12 @@ import NewsLetter from "components/NewsLetter";
 import ResourceHighlights from "components/ResourceHighlights";
 import ResourceList from "components/ResourceList";
 import { ResourcesTypes } from "components/types/types";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
+import {
+  GetServerSideProps,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
 
 export const Home = ({
   resources,
@@ -18,14 +23,29 @@ export const Home = ({
   );
 };
 
+// When you use getStaticProps, it is called only once and at build-time
 export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch("http://localhost:3001/api/resources");
   const data: ResourcesTypes[] = await response.json();
+
+  console.log("Calling getStaticProps");
   return {
     props: {
       resources: data,
     },
   };
 };
+
+// When getServerSideProps is used, it called everytime a user visits the page
+// This function call is executed on the server
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const response = await fetch("http://localhost:3000/api/resources");
+//   const data: ResourcesTypes[] = await response.json();
+//   return {
+//     props: {
+//       resources: data,
+//     },
+//   };
+// };
 
 export default Home;
